d9339c85ad68053d8719d7c5a120b2ddcfe55029
yanxuewen <yanxuewen325@163.com>
2017-02-20 21:34:48 +0800

    init

---
+++ YInkeLiveDemo/YInkeLiveDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YInkeLiveDemo
+//
+//  Created by yanxuewen on 2017/2/20.
+//  Copyright © 2017年 YXW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YInkeLiveDemo/YInkeLiveDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YInkeLiveDemo
+//
+//  Created by yanxuewen on 2017/2/20.
+//  Copyright © 2017年 YXW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
